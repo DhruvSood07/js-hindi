396d3dbc74e16a4995505f3337e0d426be0f83b9
Dhruv <142775402+DhruvSood07@users.noreply.github.com>
2024-07-01 06:34:56 +0000

    datatypes

--- Basics/test.js
+++ Basics/test.js
@@ -1,1 +1,38 @@
-console.log("Hi")
+// const accId = 123234
+// let  accemail = "dhruvsood@gmail.com"
+// var accpass = "2323"
+// accCity = "hsp"
+
+// let accState;
+
+//  accId = 2
+
+// // console.log(accId);
+
+// // accemail = "ser@gmail";
+
+// // console.log(accemail);
+
+// // accpass = "dhruv"
+
+// // console.log(accpass);
+
+// console.table([accId , accemail, accpass, accCity, accState]);
+
+// /*
+// prefer not to use var
+// because of issue in scope and functional scope
+// */
+
+"use strict"; // treat all Js code as newer version
+
+// alert( 3+3 ) we are using node Js not browser
+
+console.log(3 + 3);
+console.log("dhruv");
+
+// alt+shift+f  formatting 
+// code redability should be there
+
+//type 
+console.log(typeof null);  //tells the type of data type
