944f499d152636871d8173c3f72442bc405e91ad
Dhruv <142775402+DhruvSood07@users.noreply.github.com>
2024-07-01 08:22:31 +0000

    conversion of data types

---
+++ Basics/new1.js
@@ -0,0 +1,21 @@
+// let score ="33"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// // conversion
+
+// let change =Number(score);
+// console.log(typeof change);
+// console.log(change);
+
+/* 
+ 33 => 33
+ 33abc => NaN
+ true => 1 false=> 0
+*/
+
+let isLogged = 0;
+
+let change1 = Boolean(isLogged);
+console.log(change1);
