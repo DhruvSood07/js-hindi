2bd17285dce722bd39c7ddb93f9b64c52943837f
Dhruv <142775402+DhruvSood07@users.noreply.github.com>
2024-07-10 08:07:20 +0000

    stackandHeap

---
+++ Basics/StackandHeap.js
@@ -0,0 +1,18 @@
+let name ="Dhruv"
+
+let name1 = name;
+name1= "Sood";
+console.log(name);
+console.log(name1);
+
+let userOne= {
+    email:"dhruv@yahoo.com",
+    upi:" sexy@ybl", 
+}
+
+let userTwo = userOne;
+
+userTwo.email = "nothing@yahoo.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
