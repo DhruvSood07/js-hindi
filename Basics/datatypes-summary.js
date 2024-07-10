5010cc5fdaec4492add8744c98ff3e6ed158aaff
Dhruv <142775402+DhruvSood07@users.noreply.github.com>
2024-07-10 05:00:57 +0000

    datatypesSummary

---
+++ Basics/datatypes-summary.js
@@ -0,0 +1,22 @@
+//primitive data types
+
+//7 types 
+
+// const id= Symbol("123");
+// const anotherid= Symbol("123");
+
+// console.log(id===anotherid );
+// console.log(anotherid);
+
+const heros = ["pokkie" , "naag", "arjun"];
+
+let myObj= {
+    name:"Dhruv",
+    age:18,
+}
+
+const myFunction = function(){
+    console.log("hello");
+}
+
+console.log(typeof myFunction );
