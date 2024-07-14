fcb6aa3e1e3a0be2089f6dbae5f8a793d93145e7
Dhruv <142775402+DhruvSood07@users.noreply.github.com>
2024-07-14 15:47:18 +0000

    MathAndNumber

---
+++ numberAndmaths.js
@@ -0,0 +1,43 @@
+const score =400;
+
+const bal = new Number(500);
+// console.log(bal);
+
+const change = bal.toString();
+// console.log(change);
+
+// console.log(typeof(change));
+
+// console.log(change.length);
+// console.log(bal.toFixed(2));  //point k baad ke numbers
+
+const otherN= 123.8989;
+// console.log(otherN.toPrecision(3));
+
+const hundreds = 100000;
+
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+//          ++++++++++++++++ MATHS  ++++++++++++++++++
+
+console.log(Math);
+
+console.log(Math.abs(-4));
+
+console.log(Math.round(4.6));
+
+console.log(Math.floor(5.6));
+console.log(Math.ceil(5.6));
+
+console.log(Math.sqrt(49));
+
+console.log(Math.random());  //value lies between 0 to 1
+
+ console.log((Math.random()*10)+1);
+
+ const min = 10;
+ const max = 20;
+
+ console.log(Math.floor(Math.random() * (max-min +1))+min)
