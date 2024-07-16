cec00689750f60c4213adbb09834c7f5f76b4fb7
Dhruv <142775402+DhruvSood07@users.noreply.github.com>
2024-07-16 04:38:33 +0000

    new commit

---
+++ 02_Basics/arrays2.js
@@ -0,0 +1,11 @@
+const marvel_heros = ["thor", "dhruv", "sood"]
+const dc = ["mia", "leone", "jonny"]
+
+// marvel_heros.push(dc); 
+// console.log(marvel_heros) // array within array
+
+// console.log(marvel_heros[3][0])
+
+const all_heros = marvel_heros.concat(dc);
+console.log(all_heros) //same array within array
+
