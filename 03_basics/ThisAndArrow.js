2eb5eca418dce0eee2f44eb0b694f297b59be07a
Dhruv <142775402+DhruvSood07@users.noreply.github.com>
2024-07-19 10:43:03 +0000

    ThisandArrow

---
+++ 03_basics/ThisAndArrow.js
@@ -0,0 +1,44 @@
+// const user ={
+//     username: "dhruv",
+//     price: 999,
+
+//     welcpmeMessage: function(){
+//         console.log(`${this.username}, welcome to this website`);
+//         console.log(this); 
+//     }
+// }
+
+// // user.welcpmeMessage()
+// // user.username ="Sood"
+// // user.welcpmeMessage()
+
+
+// // console.log(this);
+
+// // function chai()
+// // {
+// //     let username ="DJ"
+// //     console.log(this.username);
+// // }
+
+// // chai()
+
+// const chai = () =>{
+//     let username ="DJ"
+//     console.log(this)
+// }
+
+// chai()
+ 
+// const addTwo =(num1, num2) =>{
+//     return num1 +num2;
+// }
+
+// const addTwo =(num1, num2) =>  num1 +num2;
+const addTwo =(num1, num2) =>  (num1 +num2)
+
+console.log(addTwo(3,5));
+
+const myArray = [2,5,6,7,8]
+
+// myArray.forEach()
